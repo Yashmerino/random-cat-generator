608901b70ae408bb6a1aafb35881c33087180285
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2022-10-27 19:36:01 +0300

    Add functionality using Cat API (#2)
    * Create JSON for API key
    
    * Add functionality

--- src/app/components/Image.jsx
+++ src/app/components/Image.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Image = (props) => {
   return (
     <div className="image-container">
-      <img src={props.img} alt="Here should be a cat :)"></img>
+      <img src={props.src} alt="Here should be a cat :)" />
     </div>
   );
 };
