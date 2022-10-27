4bd41d1b35f18805a1aac5566d60e1214f39f0bb
Bozieac Artiom <57958281+Yashmerino@users.noreply.github.com>
2022-10-27 16:01:37 +0300

    Add initial structure

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: { main: "./src/index.js" },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|gif|jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
