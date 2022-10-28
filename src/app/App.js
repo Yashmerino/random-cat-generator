eb851d3545921b7c3107b8e82f0e968a1e082f0e
Bozieac Artiom <57958281+Yashmerino@users.noreply.github.com>
2022-10-28 21:16:59 +0300

    Implement preloader

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import Image from "./components/Image.jsx";
 import Button from "./components/Button.jsx";
 import Footer from "./components/Footer.jsx";
+import preloader from "./../images/preloader.gif";
 import api_key from "./api.json";
 
 const App = (props) => {
   const [cat, setCat] = useState({});
+  const [isLoading, setLoading] = useState(false);
 
   const fetchCat = async () => {
+    setLoading(true);
+
     const response = await fetch("https://api.thecatapi.com/v1/images/search", {
       method: "GET",
       headers: {
@@ -18,16 +22,20 @@ const App = (props) => {
     const data = await response.json();
 
     while (data[0].width >= 700 || data[0].height >= 600) {
-      data[0].width /= 2;
-      data[0].height /= 2;
+      data[0].width /= 1.2;
+      data[0].height /= 1.2;
     }
 
     setCat(data[0]);
+
+    setLoading(false);
   };
 
   return (
     <div className="main-container">
-      {Object.keys(cat).length > 0 ? (
+      {isLoading ? (
+        <img src={preloader} className="preloader" />
+      ) : Object.keys(cat).length > 0 ? (
         <Image src={cat.url} width={cat.width} height={cat.height} />
       ) : (
         <h1 className="no_cats">No cats :c Press the button</h1>
